b724032fa8556cb7988f5a2e21cb9b5cd2f4ef18
Takumi Takikawa <ressa13@Takumis-MacBook-Pro.local>
2022-07-28 23:55:43 +0900

    first commit

---
+++ server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os/user"
+	"time"
+)
+
+const (
+	Pi       = 3.14
+	Username = "test_user"
+	Password = "test_pass"
+)
+
+var (
+	i    int     = 1
+	f64  float64 = 1.2
+	s    string  = "test"
+	t, f bool    = true, false
+)
+
+func foo() {
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
+}
+
+func main() {
+	fmt.Println("Hello, World!", time.Now())
+	fmt.Println(user.Current())
+
+	fmt.Println(i, f64, s, t, f)
+
+	foo()
+}
